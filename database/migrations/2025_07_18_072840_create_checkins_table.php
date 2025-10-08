a4b25db000696e44842b199c053507e0249cf654
Pradosh Adhikari <pradoshak@gmail.com>
2025-10-08 15:48:23 +0545

    first commit

---
+++ database/migrations/2025_07_18_072840_create_checkins_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('checkins', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('guest_id')->constrained('guests')->onDelete('cascade');
+            $table->foreignId('room_id')->constrained('rooms')->onDelete('cascade');
+            $table->integer('number_of_people');
+            $table->integer('number_of_nights');
+            $table->integer('total_amount');
+            $table->date('checkin_date');
+            $table->boolean('is_checked_out')->default(false);
+            $table->date('checkout_date')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('checkins');
+    }
+};
